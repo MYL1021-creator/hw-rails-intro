bfd215c8e0e02597f523c3e3880d634a52c6f246
Max Yue Li <myl1021@berkeley.edu>
2026-07-26 22:35:57 +0000

    Add movie rating filters

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,11 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+  @all_ratings = Movie.all_ratings
+  selected_ratings = params[:ratings]&.keys
+  @ratings_to_show =
+    selected_ratings.present? ? selected_ratings : @all_ratings
+  @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   # GET /movies/1 or /movies/1.json
